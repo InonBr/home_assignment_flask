23438989669a126ed3f063ab8645a15c6d0ea41d
Inon Bercu <inonbr@gmail.com>
2021-05-11 17:14:19 +0300

    on new email, test if receiver is in the db

--- models/new_massage.py
+++ models/new_massage.py
@@ -1,11 +1,32 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, validator
 from utils import get_date_time
+from flask_pymongo import PyMongo
+from main import app
+
+import os
+
+db = os.getenv("MONGODB_KEY")
+
+app.config['MONGO_URI'] = db
+mongo = PyMongo(app)
+
+CONTENT_COLLECTIONS = mongo.db.users
 
 
 class NewMassage(BaseModel):
-    sender: str
-    receiver: str
+    sender: EmailStr
+    receiver: EmailStr
     message: str
     subject: str
     read: bool = False
     creation_date: str = get_date_time()
+
+    # validate that the receiver is in the db
+    @validator('receiver', pre=True, always=True, allow_reuse=True, check_fields=False)
+    def email_validation(cls, v, values):
+        email_test = CONTENT_COLLECTIONS.find_one({"email": v})
+
+        if email_test is None:
+            raise ValueError("sorry, email does not appear in our list")
+
+        return v
