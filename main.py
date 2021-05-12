721a2479f93147521c4f5bad7e5f62817257b730
Inon Bercu <inonbr@gmail.com>
2021-05-12 13:22:29 +0300

    removed test route

--- main.py
+++ main.py
@@ -2,14 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/", methods=["GET"])
-def test():
-    return "test me"
-
-
 from routes import massage_routes
 from routes import user_routes
 
-# if __name__ == '__main__':
-#     app.run(host="localhost", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000, debug=True)
